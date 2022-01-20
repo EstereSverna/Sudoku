9c2d73f638a336c7f01a53f196d9eb03f4a9d5d1
EstereSverna <estere.sverna@gmail.com>
2022-01-20 19:50:32 +0200

    Some progress

--- Sudoku/Form1.Designer.cs
+++ Sudoku/Form1.Designer.cs
@@ -28,13 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 133);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(503, 464);
+            this.panel1.TabIndex = 0;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(536, 370);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(228, 55);
+            this.button17.TabIndex = 1;
+            this.button17.Text = "Check";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(536, 448);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(228, 57);
+            this.button18.TabIndex = 2;
+            this.button18.Text = "Clear All";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(536, 540);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(228, 57);
+            this.button19.TabIndex = 3;
+            this.button19.Text = "Show solution";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.ShowSolution_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(580, 96);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(126, 24);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Begginer";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(580, 133);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(126, 24);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Advanced";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 628);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
+
 }
 
--- Sudoku/Form1.cs
+++ Sudoku/Form1.cs
@@ -12,9 +12,176 @@ namespace Sudoku
 {
     public partial class Form1 : Form
     {
+        private int c;
+        private Cell[,] cells;
         public Form1()
         {
             InitializeComponent();
+            CreateCells();
+            NewGame();
+        }
+
+        private void CreateCells()
+        {
+            if(radioButton1.Checked)
+            {
+                c = 4;
+            }
+            else if(radioButton2.Checked)
+            {
+                c = 9;
+            }
+            cells = new Cell[c, c];
+            for (int i = 0; i < c; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    cells[i, j] = new Cell();
+                    cells[i, j].Font = new Font(SystemFonts.DefaultFont.FontFamily, 20);
+                    cells[i, j].Size = new Size(40, 40);
+                    cells[i, j].Location = new Point(i * 40, j * 40);
+                    cells[i, j].FlatStyle = FlatStyle.Flat;
+                    cells[i, j].FlatAppearance.BorderColor = Color.Black;
+                    cells[i, j].X = i;
+                    cells[i, j].Y = j;
+                    cells[i, j].KeyPress += KeyPressed;
+
+                    panel1.Controls.Add(cells[i, j]);
+                }
+            }
+        }
+
+        private void KeyPressed(object sender, KeyPressEventArgs e)
+        {
+            int value;
+            var cell = sender as Cell;
+
+            if (cell.IsLocked)
+            {
+                return;
+            }
+            else 
+            {
+                int.TryParse(e.KeyChar.ToString(), out value);
+                cell.Text = value.ToString();
+                cell.ForeColor = SystemColors.ControlDarkDark;
+            }
+        }
+
+        private void NewGame()
+        {
+            foreach (var cell in cells)
+            {
+                cell.Clear();
+            }
+
+            do
+            { 
+                findValueForNextCell(0, 0);
+            }
+            while (false);
+
+        }
+
+        Random random = new Random();
+
+        private bool findValueForNextCell(int i, int j)
+        {
+            var numsLeft = new List<int>();
+
+            for (i = 0; i < c; i++)
+            { 
+                for (int n = 1; n <= c; n++)
+                {
+                    numsLeft.Add(n);
+                }
+
+                for (j = 0; j < c; j++)
+                {
+                    int value;
+                    do
+                    {
+                        value = numsLeft[random.Next(0, numsLeft.Count)];
+                        if (IsValidNumber(value, i, j))
+                        {
+                            cells[i, j].Value = value;
+                            cells[i, j].Text = value.ToString();
+                        }
+                    } while (!IsValidNumber(value, i, j));
+
+
+                    numsLeft.Remove(value);
+                }
+            }
+            return true;
+            }
+
+        private bool IsValidNumber(int value, int x, int y)
+        {
+            for (int i = 0; i < c; i++)
+            {
+                if (i != y && cells[x, i].Value == value)
+                    return false;
+
+                if (i != x && cells[i, y].Value == value)
+                    return false;
+            }
+
+            for (int i = x - (x % (int)Math.Sqrt(c)); i < x - (x % (int)Math.Sqrt(c)) + (int)Math.Sqrt(c); i++)
+            {
+                for (int j = y - (y % (int)Math.Sqrt(c)); j < y - (y % (int)Math.Sqrt(c)) + (int)Math.Sqrt(c); j++)
+                {
+                    if (i != x && j != y && cells[i, j].Value == value)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            var wrongCells = new List<Cell>();
+
+            foreach (var cell in cells)
+            {
+                if (!string.Equals(cell.Value.ToString(), cell.Text))
+                {
+                    wrongCells.Add(cell);
+                }
+            }
+
+            if (wrongCells.Any())
+            {
+
+                wrongCells.ForEach(x => x.ForeColor = Color.Red);
+                MessageBox.Show("Wrong inputs are marked in red");
+            }
+            else
+            {
+                MessageBox.Show("You Win");
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            foreach (var cell in cells)
+            {
+                if (cell.IsLocked == false)
+                    cell.Clear();
+            }
+        }
+
+        private void ShowSolution_Click(object sender, EventArgs e)
+        {
+            foreach (var cell in cells)
+            {
+                if (cell.IsLocked)
+                {
+                    cell.IsLocked = false;
+                    cell.ForeColor = Color.Blue;
+                }
+            }
         }
     }
 }
